5f03c541c2e5727ef3ec3cf8a1a60a41c088ba48
Adriano J. Holanda <ajholanda@gmail.com>
2023-09-11 12:19:06 -0300

    remove VS Code and SNAP from installation list

--- setup/lnx.sh
+++ setup/lnx.sh
@@ -3,8 +3,4 @@
 # Mude para o nome do instalador do sistema
 PKG='apt'
 
-sudo $PKG install \
-     bison flex gcc gdb \
-     make snapd
-
-sudo snap install code
+sudo $PKG install bison flex gcc gdb make 
